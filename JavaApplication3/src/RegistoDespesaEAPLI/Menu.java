0d24ec2aa205eb59f0f3df77588ed19ec4d4e480
Antonio <1070352@isep.ipp.pt>
2013-03-15 14:36:36 +0000

    Alteraçoes a menu e regdespesaui

--- JavaApplication3/src/RegistoDespesaEAPLI/Menu.java
+++ JavaApplication3/src/RegistoDespesaEAPLI/Menu.java
@@ -1,13 +1,30 @@
 package RegistoDespesaEAPLI;
 
-public class Menu {
+import javax.swing.JOptionPane;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class Menu {
+    
     public static void main(String[] args) {
-        System.out.println("Bem-Vindo ao gestor de despesas!");
-        System.out.println("Introduza um valor");
-        System.out.println("Introduza uma Descriçao");
-    }
+         int opcao;
+         do {  
+            opcao = Integer.parseInt(javax.swing.JOptionPane.showInputDialog("" +  
+                    "Bem vindo ao gestor de despesas mais sexy do ISEP\n" +  
+                    "Escolha uma opçao:\n" +  
+                    "1 - Registar Despesa.\n" +   
+                    "2 - Sair."));  
+            switch (opcao) {  
+                case 1:  
+                    //registardespesa();
+                    JOptionPane.showMessageDialog(null, "A despesa foi registada");  
+                    break;  
+                case 7:
+                    JOptionPane.showMessageDialog(null, "Fim!");  
+                    break;  
+                default:  
+                    JOptionPane.showMessageDialog(null, "Opçao Invalida");  
+            }  
+        } while (opcao != 7);  
+    }  
 }
+
--- JavaApplication3/src/RegistoDespesaEAPLI/RegDespesaUI.java
+++ JavaApplication3/src/RegistoDespesaEAPLI/RegDespesaUI.java
@@ -4,10 +4,18 @@
  */
 package RegistoDespesaEAPLI;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Antonio
  */
 public class RegDespesaUI {
+    public void registardespesa(){
+         int val = Integer.parseInt(javax.swing.JOptionPane.showInputDialog(null, "Introduza um valor"));
+         String desc = JOptionPane.showInputDialog(null, "Introduza uma descriçao");
+         
+         JOptionPane.showInputDialog(null, "Registo Introduzido" + "Nome: " + desc +"Valor:" + val);
+    }
     
 }
